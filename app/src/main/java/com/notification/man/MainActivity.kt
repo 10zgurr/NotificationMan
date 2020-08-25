cdbe93deee009015db1efbece246ecfd08ebe4cf
Özgür Taş <ozgur186@gmail.com>
2020-08-25 22:53:30 +0300

    - chages for the app's demonstration

--- app/src/main/java/com/notification/man/MainActivity.kt
+++ app/src/main/java/com/notification/man/MainActivity.kt
@@ -3,6 +3,7 @@ package com.notification.man
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.notificationman.library.NotificationMan
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,15 +11,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        fireNotificationMan()
+        button_fire.setOnClickListener { fireNotificationMan() }
     }
 
     private fun fireNotificationMan() = NotificationMan
         .Builder(this, "com.notification.man.MainActivity") // make sure class path match with your project architecture
-        .setTitle("test title") // optional
-        .setDescription("test desc") // optional
-        .setThumbnailImageUrl("https://digit-1.com/wp-content/uploads/sites/13/2019/10/Digit-1-News-How-to-enable-the-battery-percentage-icon-in.jpg") // optional
-        .setTimeInterval(4L) // needs secs - default is 5 secs
+        .setTitle(edit_text_title.text.toString().trim()) // optional
+        .setDescription(edit_text_desc.text.toString().trim()) // optional
+        .setThumbnailImageUrl("https://storage.googleapis.com/gweb-uniblog-publish-prod/images/Android_robot.max-500x500.png") // optional
+        .setTimeInterval(edit_text_time_interval.text.toString().trim().toLong()) // needs secs - default is 5 secs
         .setNotificationType(NotificationMan.NOTIFICATION_TYPE_IMAGE) // optional - default type is TEXT
         .fire()
 }
